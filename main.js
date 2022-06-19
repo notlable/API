c45a56aeaa5cf3cd4f0efed919c59db66414f4ed
Pavlo Korinovskyi <3120362@TEB-GDANSK.PL>
2022-06-19 11:13:55 +0200

    Pobieranie dannych

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-const example ={
-    stacja: 'Szczecin',
-    godzina_pomiaru: '6',
-    temperatura: '21.1',
-};
+// const example ={
+//     stacja: 'Szczecin',
+//     godzina_pomiaru: '6',
+//     temperatura: '21.1',
+// };
+// const miasto = prompt('Podaj nazwe miasta')
+// const div = document.querySelector('div');
+let valueCity;
+const selectCity = document.querySelector('select');
 
-const url =`https://danepubliczne.imgw.pl/api/data/synop/station/jeleniagora`;
 let data = {};
 
+const getData =()=>{
+    const url =`https://danepubliczne.imgw.pl/api/data/synop/station/${valueCity}`;
 fetch(url)
     .then((response)=>{
         if (!response.ok){
@@ -15,11 +20,23 @@ fetch(url)
             return response.json()
         }
     })
-    .then (next=>{
-        showData(next);
+    .then (data=>{
+        showData(data);
     })
     .catch(err=>console.error(err))
+}
 
     const showData=(data)=>{
-        console.log((data))
+        console.log(data)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <p>Nazwa miasta - ${data.stacja}</p>
+        <p>Temperatura - ${data.temperatura}</p>
+        <p>Data- ${data.data_pomiaru}</p>
+        <p>Godzina - ${data.godzina_pomiaru}</p>
+        `
+        document.body.appendChild(div)
     }
+    selectCity.addEventListener('change',(e)=>{
+        getData();
+    })
