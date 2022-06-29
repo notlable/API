e5858128c237025b3c400c6b1e2e692292004fae
unknown <pkorinovskyi@gmail.com>
2022-06-29 23:11:04 +0200

    Poprawiona aplikacja

--- main.js
+++ main.js
@@ -1,17 +1,10 @@
-// const example ={
-//     stacja: 'Szczecin',
-//     godzina_pomiaru: '6',
-//     temperatura: '21.1',
-// };
-// const miasto = prompt('Podaj nazwe miasta')
-// const div = document.querySelector('div');
-let valueCity;
 const selectCity = document.querySelector('select');
-
+const div = document.querySelector('div');
+const body = document.querySelector('body');
 let data = {};
 
 const getData =()=>{
-    const url =`https://danepubliczne.imgw.pl/api/data/synop/station/${valueCity}`;
+    const url =`https://danepubliczne.imgw.pl/api/data/synop/station/${selectCity.value}`;
 fetch(url)
     .then((response)=>{
         if (!response.ok){
@@ -22,21 +15,47 @@ fetch(url)
     })
     .then (data=>{
         showData(data);
+        
     })
     .catch(err=>console.error(err))
 }
 
     const showData=(data)=>{
         console.log(data)
-        const div = document.createElement('div');
+        div.textContent = "";
         div.innerHTML = `
-        <p>Nazwa miasta - ${data.stacja}</p>
-        <p>Temperatura - ${data.temperatura}</p>
-        <p>Data- ${data.data_pomiaru}</p>
-        <p>Godzina - ${data.godzina_pomiaru}</p>
+        <p><b>Nazwa miasta</b> : ${data.stacja}</p>
+        <p><b>Temperatura</b> : ${data.temperatura}°C</p>
+        <p><b>Data</b> : ${data.data_pomiaru}</p>
+        <p><b>Godzina</b> : ${data.godzina_pomiaru}:00</p>
         `
         document.body.appendChild(div)
     }
     selectCity.addEventListener('change',(e)=>{
-        getData();
+        
+        if (selectCity.value == 'gdansk') {
+            body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2018/06/21/13/57/clouds-3488632__340.jpg')";
+            body.style.height= "100vh";
+            body.style.width= "100vh";
+            
+        }
+        if (selectCity.value == 'gdansk'){
+            body.style.backgroundImage= "url('https://cdn.pixabay.com/photo/2015/11/15/21/19/gdansk-1044857__340.jpg')";
+            body.style.height = "100vh";
+            body.style.width = "100vh";
+            getData();
+        }
+        if(selectCity.value == 'warszawa'){
+            body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2015/11/01/19/45/warsaw-1017468__340.jpg')";
+            body.style.height = "100vh";
+            body.style.width = "100vh";
+            getData();
+        }
+        if(selectCity.value == 'poznan') {
+            body.style.backgroundImage = "url('https://cdn.pixabay.com/photo/2013/02/22/20/01/poznan-85099__340.jpg')";
+            body.style.height = "100vh";
+            body.style.width = "100vh";
+            getData();
+        }
+        
     })
